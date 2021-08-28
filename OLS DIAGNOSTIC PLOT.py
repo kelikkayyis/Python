56809abe9e2a086648ab31ec443be16187b0378f
Kelik Isbiyantoro <87562666+kelikkayyis@users.noreply.github.com>
2021-08-28 00:55:16 -0700

    Membuat Plot Uji Asumsi
    Diagnostic Plot untuk cek uji asumsi klasik regresi

---
+++ OLS DIAGNOSTIC PLOT.py
@@ -0,0 +1,132 @@
+# Libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import scipy.stats as stats
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from statsmodels.graphics.gofplots import ProbPlot
+
+plt.style.use('seaborn') # pretty matplotlib plots
+plt.rc('font', size=14)
+plt.rc('figure', titlesize=18)
+plt.rc('axes', labelsize=15)
+plt.rc('axes', titlesize=18)
+
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+# Data
+from sklearn.datasets import load_boston
+boston = load_boston()
+X = pd.DataFrame(boston.data, columns=boston.feature_names)
+y = pd.DataFrame(boston.target)
+
+# generate OLS model
+model = sm.OLS(y, sm.add_constant(X))
+model_fit = model.fit()
+# print(model_fit.summary())
+
+# create dataframe from X, y for easier plot handling
+dataframe = pd.concat([y, X], axis=1)
+# print('\n', dataframe)
+
+# Asumtion Diagnostic
+## Residuals vs Fitted Plot
+# model values
+model_fitted_y = model_fit.fittedvalues
+# model residuals
+model_residuals = model_fit.resid
+# normalized residuals
+model_norm_residuals = model_fit.get_influence().resid_studentized_internal
+# absolute squared normalized residuals
+model_norm_residuals_abs_sqrt = np.sqrt(np.abs(model_norm_residuals))
+# absolute residuals
+model_abs_resid = np.abs(model_residuals)
+# leverage, from statsmodels internals
+model_leverage = model_fit.get_influence().hat_matrix_diag
+# cook's distance, from statsmodels internals
+model_cooks = model_fit.get_influence().cooks_distance[0]
+
+# plot_lm_1 = plt.subplot(2, 2, 1)
+plot_lm_1 = plt.figure(1)
+sns.residplot(model_fitted_y, dataframe.columns[-1], data=dataframe,
+                          lowess=True,
+                          scatter_kws={'alpha': 0.5},
+                          line_kws={'color': 'red', 'lw': 1, 'alpha': 0.8})
+plot_lm_1.axes[0].set_title('Residuals vs Fitted')
+plot_lm_1.axes[0].set_xlabel('Fitted values')
+plot_lm_1.axes[0].set_ylabel('Residuals');
+# plt.show()
+
+# Normal Q-Q Plot
+# plot_lm_3 = plt.subplot(2, 2, 3)
+plot_lm_2 = plt.figure(2)
+sorted_student_residuals = pd.Series(model_fit.get_influence().resid_studentized_internal)
+sorted_student_residuals.index = model_fit.resid.index
+sorted_student_residuals = sorted_student_residuals.sort_values(ascending = True)
+df = pd.DataFrame(sorted_student_residuals)
+df.columns = ['sorted_student_residuals']
+df['theoretical_quantiles'] = stats.probplot(df['sorted_student_residuals'], dist = 'norm', fit = False)[0]
+rankings = abs(df['sorted_student_residuals']).sort_values(ascending = False)
+top3 = rankings[:3]
+
+x = df['theoretical_quantiles']
+y = df['sorted_student_residuals']
+plt.scatter(x,y,  alpha=0.5)
+plot_lm_2.axes[0].set_title('Normal Q-Q')
+plot_lm_2.axes[0].set_ylabel('Standardized Residuals')
+plot_lm_2.axes[0].set_xlabel('Theoretical Quantiles')
+plot_lm_2.axes[0].plot([np.min([x,y]),np.max([x,y])],[np.min([x,y]),np.max([x,y])], color = 'r')
+for val in top3.index:
+    plot_lm_2.axes[0].annotate(val,xy=(df['theoretical_quantiles'].loc[val],df['sorted_student_residuals'].loc[val]))
+# plt.show()
+
+# Scale-Location
+# plot_lm_3 = plt.subplot(2, 2, 3)
+plot_lm_3 = plt.figure(3)
+plt.scatter(model_fitted_y, model_norm_residuals_abs_sqrt, alpha=0.5);
+sns.regplot(model_fitted_y, model_norm_residuals_abs_sqrt,
+              scatter=False,
+              ci=False,
+              lowess=True,
+              line_kws={'color': 'red', 'lw': 1, 'alpha': 0.8});
+plot_lm_3.axes[0].set_title('Scale-Location')
+plot_lm_3.axes[0].set_xlabel('Fitted values')
+plot_lm_3.axes[0].set_ylabel('$\sqrt{|Standardized Residuals|}$');
+
+# annotations
+abs_norm_resid = np.flip(np.argsort(np.abs(model_norm_residuals)), 0)
+abs_norm_resid_top_3 = abs_norm_resid[:3]
+abs_sq_norm_resid = np.flip(np.argsort(model_norm_residuals_abs_sqrt), 0)
+abs_sq_norm_resid_top_3 = abs_sq_norm_resid[:3]
+for i in abs_norm_resid_top_3:
+      plot_lm_3.axes[0].annotate(i,
+                                 xy=(model_fitted_y[i],
+                                     model_norm_residuals_abs_sqrt[i]));
+# plt.show()
+
+# Residuals vs Leverage
+# plot_lm_4 = plt.subplot(2, 2, 4)
+plot_lm_4 = plt.figure(4)
+plt.scatter(model_leverage, model_norm_residuals, alpha=0.5);
+sns.regplot(model_leverage, model_norm_residuals,
+              scatter=False,
+              ci=False,
+              lowess=True,
+              line_kws={'color': 'red', 'lw': 1, 'alpha': 0.8});
+plot_lm_4.axes[0].set_xlim(0, max(model_leverage)+0.01)
+plot_lm_4.axes[0].set_ylim(-3, 5)
+plot_lm_4.axes[0].set_title('Residuals vs Leverage')
+plot_lm_4.axes[0].set_xlabel('Leverage')
+plot_lm_4.axes[0].set_ylabel('Standardized Residuals');
+
+# annotations
+leverage_top_3 = np.flip(np.argsort(model_cooks), 0)[:3]
+for i in leverage_top_3:
+      plot_lm_4.axes[0].annotate(i,
+                                 xy=(model_leverage[i],
+                                     model_norm_residuals[i]));
+plt.show()
+
+# solve the bugs normal qq use sm plot
