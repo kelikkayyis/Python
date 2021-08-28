b1451425201ddd01385b0e5d9133748723ba5aef
Kelik Isbiyantoro <87562666+kelikkayyis@users.noreply.github.com>
2021-08-28 01:09:45 -0700

    Update CombineExcelFile.py
    menambahkan penyesuaian format data sesuai simpeg

--- CombineExcelFile.py
+++ CombineExcelFile.py
@@ -20,5 +20,30 @@ print(df.shape)
 print("="*11)
 print(414*13)
 
+# merapikan format & menggabungkan dengan id pegawai
+
+# import data id
+os.chdir('/content/drive/MyDrive/KSO SCISI/Absensi 2021/')
+import pandas as pd
+ID = pd.read_excel('Nama & ID Absensi.xlsx', skiprows=[0])
+
+# menggabungkan absen dengan ID
+merged_df = pd.merge(df, ID, 
+                     left_on = 'Nama', 
+                     right_on = 'NOTE', 
+                     how='left')
+
+# membuat fitur hari dan shift
+import numpy as np
+merged_df['Day'] = merged_df['Date'].dt.day_name() # membuat berdarsarkan hari
+merged_df['Shift'] = np.where(merged_df['Day']!= 'Sunday', 'ST01', 'NW')  # != berarti tidak sama
+merged_df = merged_df.sort_values(by=['ID','Nama','Date'], 
+                                  ascending=True) # mengurutkann berdasarkan nama & tanggal
+
+# menyesuaikan dengan format simpeg
+df_format = merged_df[['ID','Shift','Day','Date','Time One','Date','Time Off','Nama']]
+df_format = df_format[~df_format['ID'].isna()] # membuang pegawai yang tidak memiliki ID (~ 'negasi')
+df_format.head(7) # melihat hasil file sudah rapi
+
 # menyimpan file hasil penggabungan
-df.to_excel('Juli_01to13_2021.xls')
+df_format.to_excel('Agustus_01to22_2021.xls') # ganti nama sesuai bulannya
